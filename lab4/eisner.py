e4062232c0feb08c60b12da83c5994906e5d7c41
Michael Sörsäter <micso554@student.liu.se>
2017-02-24 11:12:58 +0100

    MAJOR refactoring.

--- lab4/eisner.py
+++ lab4/eisner.py
@@ -1,10 +1,14 @@
 import sys
+'''
+    Run code with: python3 eisner.py my_rad_file.conllu
+    Writes projective trees to my_rad_file.conllu_projective
+'''
 
 INF = sys.maxsize
-
 tree = []
 trees = []
 
+# Reading trees from the given file
 filename = sys.argv[1]
 with open(filename) as f:
     for line in f:
@@ -22,11 +26,16 @@ with open(filename) as f:
 
     trees.append(tree[:])
 
+total = len(trees)-2
 for tree_idx, tree in enumerate(trees):
+    if not tree:
+        continue
+    print("Progress: {0:.2f}".format(100*tree_idx/total), end="\r")
     n = len(tree)
     heads = [int(line[6]) for line in tree]
-    A = [[-INF for i in range(n+1)] for j in range(n+1)]
 
+    # Score matrix
+    A = [[INF for i in range(n+1)] for j in range(n+1)]
     for child in tree:
         idx = int(child[0])
         parent = int(child[6])
@@ -34,76 +43,70 @@ for tree_idx, tree in enumerate(trees):
         while parent > 0:
             A[parent][idx] = cnt
             parent = heads[parent-1]
-            cnt -= 1
+            cnt += 1
         A[0][idx] = cnt
 
-    T1 = [[-INF for i in range(n+1)] for j in range(n+1)]
-    T2 = [[-INF for i in range(n+1)] for j in range(n+1)]
-    T3 = [[-INF for i in range(n+1)] for j in range(n+1)]
-    T4 = [[-INF for i in range(n+1)] for j in range(n+1)]
+    T1 = [[INF if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
+    T2 = [[INF if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
+    T3 = [[INF if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
+    T4 = [[INF if i!=j else 0 for i in range(n+1)] for j in range(n+1)]
 
-    for i in range(len(T1)):
-        T1[i][i] = 0
-        T2[i][i] = 0
-        T3[i][i] = 0
-        T4[i][i] = 0
-
+    # Backtracking
     t1 = {}
     t2 = {}
     t3 = {}
     t4 = {}
 
+    # Following the algorithm given in the hand out
     for m in range(1, n + 1):
         for s in range(n + 1):
             t = s+m
             if t > n:
-                break;
+                break
 
-            best_q = -1
+            best_q3 = -1
+            best_q4 = -1
             for q in range(s,t):
-                tmp = T2[s][q] + T1[q+1][t] + A[s][t]
-                if tmp > T3[s][t]:
-                    T3[s][t] = tmp
-                    best_q = q
-            if best_q != -1:
-                t3[(s,t)] = t2.get((s, best_q), []) + t1.get((best_q + 1,t), []) + [(s,t)]
+                tmp3 = T2[s][q] + T1[q+1][t] + A[s][t]
+                tmp4 = T2[s][q] + T1[q+1][t] + A[t][s]
+                if tmp3 < T3[s][t]:
+                    T3[s][t] = tmp3
+                    best_q3 = q
+                if tmp4 < T4[s][t]:
+                    best_q4 = q
+                    T4[s][t] = tmp4
+            if best_q3 != -1:
+                t3[(s,t)] = t2.get((s, best_q3), []) + t1.get((best_q3 + 1,t), []) + [(s,t)]
+            if best_q4 != -1:
+                t4[(s,t)] = t2.get((s, best_q4), []) + t1.get((best_q4 + 1, t), []) + [(t,s)]
 
-            best_q = -1
+            best_q1 = -1
+            best_q2 = -1
             for q in range(s,t):
-                tmp = T2[s][q] + T1[q+1][t] + A[t][s]
-                if tmp > T4[s][t]:
-                    T4[s][t] = tmp
-                    best_q = q
-            if best_q != -1:
-                t4[(s,t)] = t2.get((s, best_q), []) + t1.get((best_q + 1, t), []) + [(t,s)]
-
-            best_q = -1
-            for q in range(s,t):
-                tmp = T2[s][q] + T3[q][t]
-                if tmp > T2[s][t]:
-                    T2[s][t] = tmp
-                    best_q = q
-            if best_q != -1:
-                t2[(s,t)] = t2.get((s, best_q), []) + t3.get((best_q,t), [])
-
-            best_q = -1
-            for q in range(s,t):
-                q2 = q + 1
-                tmp = T4[s][q2] + T1[q2][t]
-                if tmp > T1[s][t]:
-                    T1[s][t] = tmp
-                    best_q = q2
-            if best_q != -1:
-                t1[(s,t)] = t4.get((s, best_q), []) + t1.get((best_q,t), [])
+                tmp1 = T4[s][q + 1] + T1[q + 1][t]
+                tmp2 = T2[s][q] + T3[q][t]
+                if tmp1 < T1[s][t]:
+                    T1[s][t] = tmp1
+                    best_q1 = q + 1
+                if tmp2 < T2[s][t]:
+                    T2[s][t] = tmp2
+                    best_q2 = q
+            if best_q1 != -1:
+                t1[(s,t)] = t4.get((s, best_q1), []) + t1.get((best_q1,t), [])
+            if best_q2 != -1:
+                t2[(s,t)] = t2.get((s, best_q2), []) + t3.get((best_q2,t), [])
 
     # The total cost for lifting
-    #print("Cost:", -T2[0][n])
+    #print("Cost:", T2[0][n])
 
-    # Update with new heads
-    for head, idx in t2[(0, n)]:
-        trees[tree_idx][idx-1][6] = str(head)
+    # If cost is pos
+    if T2[0][n] > 0:
+        # Update with new heads
+        for head, idx in t2[(0, n)]:
+            trees[tree_idx][idx-1][6] = str(head)
 
 # Save projective trees
+print("Writing to file: " + filename + '_projective')
 with open(filename + '_projective', 'w') as f:
     for t in trees:
         for hej in t:
